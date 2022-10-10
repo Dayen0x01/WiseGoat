9214824c0a55ff058901d202b7bfb90e4b5345ef
Dayen0x01 <neydame2017@gmail.com>
2022-10-10 16:58:31 -0300

    fix request

--- assets/js/advice.js
+++ assets/js/advice.js
@@ -2,11 +2,26 @@ var elementButton = document.getElementById("getAdvice");
 var elementAdvice = document.getElementsByClassName("advice")[0];
 
 elementButton.addEventListener("click", function(e) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://api.adviceslip.com/advice", false ); 
-    xmlHttp.send( null );
-    let json = JSON.parse(xmlHttp.responseText);
-    let advice = json.slip.advice;
-    
-    elementAdvice.innerHTML = advice;
+    getAdvice();
 });
+
+async function getAdvice()
+{
+    let advice = await getData();
+    elementAdvice.innerHTML = advice;
+}
+async function getData()
+{
+    let response = await fetch('https://api.adviceslip.com/advice', {cache: "no-store"})
+    .then((response) => response.json())
+    .then((data) => {
+        return data.slip.advice;
+    });
+
+    return response;
+}
+
+async function translate(text)
+{
+    return "translated: " + text;
+}
